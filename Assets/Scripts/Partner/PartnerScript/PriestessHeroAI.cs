82e0732817e4c0f74b6eddd7b3a497d1e88ea556
Stellar Survivor <suppachok.suphakat@gmail.com>
2024-10-22 19:27:02 +0700

    Priestess Begin

---
+++ Assets/Scripts/Partner/PartnerScript/PriestessHeroAI.cs
@@ -0,0 +1,417 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PriestessHeroAI : MonoBehaviour
+{
+    [Header("Status")]
+    public float attackDistance = 5f;
+    public float distanceToAttack = 1f;
+    public int followSpeed = 2;
+    //public int chaseSpeed = 2;
+    //public float maxChaseDistance = 10f;
+    //public int retreatSpeed = 3;
+    //public float retreatDistance = 3f;
+    //public float retreatDuration = 1f;
+    //private float retreatTimer = 0f;
+    //public float safeDistance = 6f;
+
+    public enum State
+    {
+        follow = 0,
+        chase = 1,
+        attack = 2,
+        skill = 3,
+        retreat = 4,
+        death = 5,
+    }
+
+    public Transform player;
+    //public Status status;
+    public State currentState = State.follow;
+
+
+    //RaycastHit2D hit;
+    public LayerMask focus;
+
+    //==Combat==//
+    private Knockback knockback;
+    private Flash flash;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private Transform arrowSpawnPoint;
+    public bool isAttacking = false;
+
+    public Transform enemyTransform;
+    public Transform focusEnemy;
+
+    [SerializeField] private float cooldownTime;
+    private float attackCooldown;
+    [SerializeField] private bool isCooldown;
+
+    ArcherHeroSkill skill;
+
+    [SerializeField] private EnemyHealth currentEnemyHealth;
+
+    [SerializeField] private float slideDuration = 0.5f;  // Duration for sliding past obstacles
+    private bool isSliding = false;
+
+    private LineTrigger lineTrigger;
+
+    [SerializeField] public GameObject skillBar;
+    [SerializeField] public GameObject weaponBar;
+
+    public ArcherHeroSkill archerHeroSkill;
+
+    private void Awake()
+    {
+        flash = GetComponent<Flash>();
+        knockback = GetComponent<Knockback>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+
+        attackCooldown = cooldownTime;
+        skill = gameObject.GetComponent<ArcherHeroSkill>();
+
+        archerHeroSkill = GetComponent<ArcherHeroSkill>();
+        lineTrigger = GameObject.Find("Player").GetComponent<LineTrigger>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTime -= Time.deltaTime;
+
+        HandleMouseInput();
+
+        if (ShouldRetreat())
+        {
+            currentState = State.retreat;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentState = State.follow;
+            focusEnemy = null;
+            animator.SetBool("isWalking", false);
+        }
+
+        switch (currentState)
+        {
+            case State.follow:
+                FollowLogic();
+                break;
+            case State.chase:
+                //ChaseLogic();
+                break;
+            case State.attack:
+                AttackLogic();
+                break;
+            case State.retreat:
+                RetreatLogic();
+                break;
+            case State.skill:
+                SkillLogic();
+                break;
+            case State.death:
+                break;
+        }
+    }
+
+    void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(3))
+        {
+            if (lineTrigger.currentTarget == this.transform)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0f;
+
+                float detectionRadius = 0.5f; // Adjust this radius as needed
+                Collider2D hit = Physics2D.OverlapCircle(mousePosition, detectionRadius);
+
+                if (hit != null)
+                {
+                    Debug.Log("Collider detected: " + hit.name);
+                    if (hit.CompareTag("Enemy"))
+                    {
+                        // Hide the arrow on the previous enemy if it exists
+                        if (currentEnemyHealth != null)
+                        {
+                            Debug.Log("Hiding arrow on previous enemy: " + currentEnemyHealth.name);
+                            currentEnemyHealth.HideArrow();
+                        }
+
+                        // Set the new enemy and show the arrow
+                        Debug.Log("Enemy detected: " + hit.name);
+                        focusEnemy = hit.transform;
+                        enemyTransform = focusEnemy;
+                        currentState = State.chase;
+
+                        // Update the currentEnemyHealth reference
+                        currentEnemyHealth = hit.GetComponent<EnemyHealth>();
+                        if (currentEnemyHealth != null)
+                        {
+                            Debug.Log("Showing arrow on new enemy: " + currentEnemyHealth.name);  // This log should now appear
+                            currentEnemyHealth.ShowArrow();
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("Detected but not an enemy: " + hit.name);
+                    }
+                }
+                else
+                {
+                    Debug.Log("No collider detected within radius.");
+                }
+            }
+        }
+    }
+    void FollowLogic()
+    {
+        FlipSprite(PlayerController.instance.transform);
+        float distancePlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distancePlayer > 2.5f)
+        {
+            animator.SetBool("isWalking", true);
+            Vector2 targetPosition = new Vector2(player.transform.position.x, player.transform.position.y);
+            Vector2 directionToPlayer = (targetPosition - (Vector2)transform.position).normalized;
+
+            if (!isSliding && !CanMove(directionToPlayer))
+            {
+                StartCoroutine(SlidePastObstacle(directionToPlayer));
+            }
+            else
+            {
+                transform.position = Vector2.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (currentState == State.skill) return;
+
+        if (other.CompareTag("Enemy"))
+        {
+            focusEnemy = other.transform;
+            enemyTransform = focusEnemy;
+
+            currentState = State.chase;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (currentState == State.skill) return;
+
+        if (other.CompareTag("Enemy"))
+        {
+            focusEnemy = other.transform;
+            enemyTransform = focusEnemy;
+
+            currentState = State.chase;
+        }
+    }
+
+    private void ChaseLogic()
+    {
+        if (currentState == State.skill) return;
+
+        if (enemyTransform != null)
+        {
+            // Calculate distance to the enemy and player
+            float distanceToEnemy = Vector2.Distance(transform.position, enemyTransform.position);
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+            // If the distance to the player exceeds the max chase distance, return to follow
+            if (distanceToPlayer > maxChaseDistance)
+            {
+                currentState = State.follow;
+                focusEnemy = null; // Optionally, clear the enemy focus
+                return;
+            }
+
+            Vector3 directionToEnemy = (enemyTransform.position - transform.position).normalized;
+
+            // Stop the walking animation if within attack range but on cooldown
+            if (distanceToEnemy <= attackDistance && cooldownTime > 0f)
+            {
+                // Within attack range but still on cooldown, stop the walking animation
+                animator.SetBool("isWalking", false);
+            }
+            else
+            {
+                FlipSprite(enemyTransform);
+                // Continue walking if not within attack range or if not on cooldown
+                animator.SetBool("isWalking", true);
+
+                if (distanceToEnemy > attackDistance)
+                {
+                    // Move towards the enemy
+                    if (!isSliding && !CanMove(directionToEnemy))
+                    {
+                        StartCoroutine(SlidePastObstacle(directionToEnemy));
+                    }
+                    else
+                    {
+                        transform.Translate(directionToEnemy * chaseSpeed * Time.deltaTime);
+                    }
+                }
+                else
+                {
+                    AttackLogic(); // Start attacking if close enough
+                }
+            }
+        }
+        else
+        {
+            currentState = State.follow;
+        }
+    }
+
+    private bool CanMove(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 0.1f, LayerMask.GetMask("Obstacles"));
+        return hit.collider == null;
+    }
+
+    private IEnumerator SlidePastObstacle(Vector2 direction)
+    {
+        isSliding = true;
+        float slideTime = 0;
+
+        while (slideTime < slideDuration)
+        {
+            transform.Translate(direction * followSpeed * Time.deltaTime);
+            slideTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isSliding = false;
+    }
+
+    void AttackLogic()
+    {
+        if (enemyTransform != null && cooldownTime <= 0f)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemyTransform.position);
+
+            if (distanceToEnemy > retreatDistance)
+            {
+                animator.SetBool("isWalking", false);
+                FlipSprite(enemyTransform);
+                animator.SetTrigger("attack");
+                cooldownTime = attackCooldown;
+            }
+            else
+            {
+                currentState = State.retreat;
+            }
+        }
+        else
+        {
+            currentState = State.chase;
+        }
+    }
+
+    bool ShouldRetreat()
+    {
+        if (enemyTransform != null)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemyTransform.position);
+            // If the enemy is within retreat distance, trigger retreat
+            return distanceToEnemy < retreatDistance;
+        }
+        return false;
+    }
+
+    void RetreatLogic()
+    {
+        if (enemyTransform != null)
+        {
+            float distanceToEnemy = Vector2.Distance(transform.position, enemyTransform.position);
+
+            // Start or continue retreating
+            if (distanceToEnemy < retreatDistance)
+            {
+                // Reset retreat timer if too close to enemy
+                retreatTimer = retreatDuration;
+            }
+
+            // Countdown retreat timer
+            if (retreatTimer > 0)
+            {
+                retreatTimer -= Time.deltaTime;
+
+                // Move away from the enemy
+                Vector3 directionAwayFromEnemy = (transform.position - enemyTransform.position).normalized;
+                transform.Translate(directionAwayFromEnemy * retreatSpeed * Time.deltaTime);
+                animator.SetBool("isWalking", true);
+            }
+            else if (distanceToEnemy > safeDistance)
+            {
+                // Once safe distance is reached and timer ends, go back to attack
+                currentState = State.attack;
+            }
+            else
+            {
+                // Prevent rapid switching; stay in retreat until the timer ends
+                retreatTimer = retreatDuration;
+            }
+        }
+    }
+
+    public void SkillLogic()
+    {
+        //if (skill.barrierCircleInstance != null)
+        //{
+
+        //}
+        //else
+        //{
+        //    currentState = State.follow;
+        //}
+    }
+
+    public void Attack()
+    {
+        // Calculate direction towards the enemy
+        Vector2 directionToEnemy = (enemyTransform.position - transform.position).normalized;
+
+        // Calculate rotation angle towards the enemy
+        float angle = Mathf.Atan2(directionToEnemy.y, directionToEnemy.x) * Mathf.Rad2Deg;
+        arrowSpawnPoint.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        GameObject newArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, arrowSpawnPoint.rotation);
+        newArrow.GetComponent<Arrow>().UpdateProjectileRange(12f);
+    }
+
+    private void Unattacked()
+    {
+        //damageCollider.gameObject.SetActive(false);
+    }
+
+    void FlipSprite(Transform flipTo)
+    {
+        if (flipTo.transform.position.x < transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+    }
+}
---
+++ Assets/Scripts/Partner/PartnerScript/PriestessHeroSkill.cs
@@ -0,0 +1,332 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PriestessHeroSkill : MonoBehaviour
+{
+    [Header("Partner Skill")]
+    [SerializeField] private GameObject skillPrefab;
+    [SerializeField] private GameObject skill2Prefab;
+
+    [Header("Skill 1")]
+    [SerializeField] int skill1CooldownTime;
+    [SerializeField] int skill1MaxCooldownTime;
+    [SerializeField] int skill1CurrentCooldownTime;
+    [SerializeField] float skill1ActiveTime;
+    [SerializeField] float currentSkill1ActiveTime;
+
+    [Header("Skill 2")]
+    [SerializeField] int skill2CooldownTime;
+    [SerializeField] int skill2MaxCooldownTime;
+    [SerializeField] int skill2CurrentCooldownTime;
+    [SerializeField] float skill2ActiveTime;
+    [SerializeField] float currentSkill2ActiveTime;
+
+    [SerializeField] float cooldownRecoveryTimer = 1;
+    [SerializeField] float cooldownRecoveryDelay = 0.1f;
+
+    private bool fusionActivated = false;
+
+    [SerializeField] private StatusBar statusComponent;
+    PartnerSkillManager partnerSkillManager;
+
+    [Header("Skill Change")]
+    [SerializeField] public ToolbarSlot toolbarSlot;
+    [SerializeField] public WeaponInfo weaponInfo;
+    [SerializeField] public Sprite itemSprite;
+
+    WeaponInfo weaponChangeInfo;
+    Sprite weaponChangeSprite;
+    public ActiveToolbar activeToolbar;
+
+    [SerializeField] public Image skill1Image;
+    [SerializeField] public Image skill2Image;
+
+    private LineTrigger lineTrigger;
+
+    enum AbilityState
+    {
+        ready,
+        active1,
+        active2,
+        fusion,
+        cooldown
+    }
+    AbilityState state = AbilityState.cooldown;
+
+    private void Awake()
+    {
+        lineTrigger = GameObject.Find("Player").GetComponent<LineTrigger>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        partnerSkillManager = GameObject.Find("PartnerCanvas").GetComponent<PartnerSkillManager>();
+
+        skill1CurrentCooldownTime = skill1CooldownTime;
+        skill2CurrentCooldownTime = skill2CooldownTime;
+
+        statusComponent = GameObject.Find("ArcherPartnerSkill").GetComponent<StatusBar>();
+        GameObject.Find("ArcherSkillBubble").SetActive(false);
+
+        skill1Image.fillAmount = 0;
+        //skill2Image.fillAmount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case AbilityState.ready:
+                //GameObject.Find("ArcherSkillBubble").SetActive(true);
+                gameObject.GetComponent<Animator>().ResetTrigger("skill");
+                if (lineTrigger.currentTarget == this.transform)
+                {
+                    if (Input.GetKeyDown(KeyCode.Q))
+                    {
+                        SkillActivate();
+                        state = AbilityState.active1;
+                        skill1ActiveTime = currentSkill1ActiveTime;
+                    }
+                    else if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        Skill2Activate();
+                        statusComponent.Set(0, skill2MaxCooldownTime);
+                        state = AbilityState.active2;
+                        skill2ActiveTime = currentSkill2ActiveTime;
+                    }
+                }
+                break;
+            case AbilityState.active1:
+                //GameObject.Find("ArcherSkillBubble").SetActive(false);
+
+                if (skill1ActiveTime >= 0)
+                {
+                    skill1ActiveTime -= Time.deltaTime;
+                }
+                else
+                {
+                    transform.parent.localScale = new Vector3(1f, 1f, 1f);
+
+                    //if (weaponChangeInfo != null)
+                    //{
+                    //    toolbarSlot.weaponInfo = weaponChangeInfo;
+                    //    toolbarSlot.slotSprite.GetComponent<Image>().sprite = weaponChangeSprite;
+                    //}
+                    //else
+                    //{
+                    //    toolbarSlot.weaponInfo = null;
+                    //    toolbarSlot.slotSprite.GetComponent<Image>().sprite = weaponChangeSprite;
+                    //}
+
+                    //GameObject.Find("ActiveToolbar").GetComponent<ActiveToolbar>().ChangeActiveWeapon();
+
+                    state = AbilityState.cooldown;
+                    skill1CooldownTime = skill1CurrentCooldownTime;
+                }
+                break;
+            case AbilityState.active2:
+                if (skill2ActiveTime >= 0)
+                {
+                    skill2ActiveTime -= Time.deltaTime;
+                }
+                else
+                {
+                    //// Reset the weapon if necessary
+                    //if (weaponChangeInfo != null)
+                    //{
+                    //    toolbarSlot.weaponInfo = weaponChangeInfo;
+                    //    toolbarSlot.slotSprite.GetComponent<Image>().sprite = weaponChangeSprite;
+                    //}
+                    //else
+                    //{
+                    //    toolbarSlot.weaponInfo = null;
+                    //    toolbarSlot.slotSprite.GetComponent<Image>().sprite = weaponChangeSprite;
+                    //}
+
+                    //GameObject.Find("ActiveToolbar").GetComponent<ActiveToolbar>().ChangeActiveWeapon();
+
+                    state = AbilityState.cooldown;
+                    skill2CooldownTime = skill2CurrentCooldownTime; // Reset skill2CooldownTime
+                }
+                break;
+            case AbilityState.fusion:
+                /////////////////////////////////
+                break;
+            case AbilityState.cooldown:
+                if (skill1CooldownTime < skill1MaxCooldownTime)
+                {
+                    Skill1CooldownOverTime();
+                }
+                if (skill2CooldownTime < skill2MaxCooldownTime)
+                {
+                    Skill2CooldownOverTime();
+                }
+                if (skill1CooldownTime >= skill1MaxCooldownTime &&
+                    skill2CooldownTime >= skill2MaxCooldownTime)
+                {
+                    state = AbilityState.ready;
+                }
+                UpdateCooldownUI(); // Update the UI for all skills
+                break;
+        }
+    }
+
+    public void Skill1CooldownOverTime()
+    {
+        if (cooldownRecoveryTimer <= 0)
+        {
+            skill1CooldownTime += 1;
+            cooldownRecoveryTimer = cooldownRecoveryDelay;
+        }
+        else
+        {
+            cooldownRecoveryTimer -= Time.deltaTime;
+        }
+        skill1CooldownTime = Mathf.Min(skill1CooldownTime, skill1MaxCooldownTime);
+    }
+
+    public void Skill2CooldownOverTime()
+    {
+        if (cooldownRecoveryTimer <= 0)
+        {
+            skill2CooldownTime += 1;
+            cooldownRecoveryTimer = cooldownRecoveryDelay;
+        }
+        else
+        {
+            cooldownRecoveryTimer -= Time.deltaTime;
+        }
+        skill2CooldownTime = Mathf.Min(skill2CooldownTime, skill2MaxCooldownTime);
+    }
+
+    public void SkillActivate()
+    {
+        Debug.Log("Partner Skill Activate");
+        gameObject.GetComponent<Animator>().SetTrigger("skill");
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+        Instantiate(skillPrefab, mousePosition, Quaternion.identity);
+
+        //if (toolbarSlot.weaponInfo != null)
+        //{
+        //    weaponChangeInfo = toolbarSlot.weaponInfo;
+        //}
+        //weaponChangeSprite = toolbarSlot.slotSprite.GetComponent<Image>().sprite;
+
+        skill1CooldownTime = 0;  // Start cooldown
+        UpdateCooldownUI();  // Update UI
+    }
+
+    public void Skill2Activate()
+    {
+        Debug.Log("Partner Skill 2 Activate");
+        gameObject.GetComponent<Animator>().SetTrigger("skill");
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+        Instantiate(skill2Prefab, mousePosition, Quaternion.identity);
+
+        //if (toolbarSlot.weaponInfo != null)
+        //{
+        //    weaponChangeInfo = toolbarSlot.weaponInfo;
+        //}
+        //weaponChangeSprite = toolbarSlot.slotSprite.GetComponent<Image>().sprite;
+
+        skill2CooldownTime = 0;  // Start cooldown for Skill 2
+        UpdateCooldownUI();  // Update UI
+    }
+
+    public void FusionActivate()
+    {
+        PlayerController.instance.GetComponent<Animator>().SetTrigger("ArcherFusion");
+
+        if (toolbarSlot != null)
+        {
+            toolbarSlot.gameObject.SetActive(true); // Ensure toolbarSlot is active
+            StartCoroutine(EnsureUIUpdate()); // Ensure UI is updated
+        }
+
+        if (toolbarSlot.weaponInfo != null)
+        {
+            weaponChangeInfo = toolbarSlot.weaponInfo;
+        }
+        weaponChangeSprite = toolbarSlot.slotSprite.GetComponent<Image>().sprite;
+
+        toolbarSlot.weaponInfo = weaponInfo;
+        toolbarSlot.slotSprite.GetComponent<Image>().sprite = itemSprite;
+        activeToolbar.ChangeActiveWeapon();
+
+        fusionActivated = true;
+    }
+
+    public IEnumerator EnsureUIUpdate()
+    {
+        yield return null; // Wait for a frame
+        if (toolbarSlot != null && toolbarSlot.gameObject.activeInHierarchy)
+        {
+            var imageComponent = toolbarSlot.slotSprite.GetComponent<Image>();
+            if (imageComponent != null)
+            {
+                imageComponent.sprite = itemSprite;
+            }
+        }
+    }
+
+    public void DeFusionActivate()
+    {
+        // Trigger return to normal state
+        PlayerController.instance.GetComponent<Animator>().SetTrigger("ArcherFusionReturn");
+
+        // Handle weapon change back
+        if (weaponChangeInfo != null)
+        {
+            toolbarSlot.weaponInfo = weaponChangeInfo;
+            toolbarSlot.slotSprite.GetComponent<Image>().sprite = weaponChangeSprite;
+        }
+        else
+        {
+            toolbarSlot.weaponInfo = null;
+            toolbarSlot.slotSprite.GetComponent<Image>().sprite = weaponChangeSprite;
+        }
+
+        activeToolbar.ChangeActiveWeapon();
+
+        StartCoroutine(ResetFusionTrigger());
+
+        fusionActivated = false;
+    }
+
+    IEnumerator ResetFusionTrigger()
+    {
+        yield return new WaitForSeconds(0.1f);
+        PlayerController.instance.GetComponent<Animator>().ResetTrigger("ArcherFusionReturn");
+    }
+
+    private void UpdateCooldownUI()
+    {
+        // Update Skill 1 UI
+        if (skill1CooldownTime < skill1MaxCooldownTime)
+        {
+            skill1Image.fillAmount = skill1CooldownTime / (float)skill1MaxCooldownTime;
+        }
+        else
+        {
+            skill1Image.fillAmount = 1;
+        }
+
+        // Update Skill 2 UI
+        if (skill2CooldownTime < skill2MaxCooldownTime)
+        {
+            skill2Image.fillAmount = skill2CooldownTime / (float)skill2MaxCooldownTime;
+        }
+        else
+        {
+            skill2Image.fillAmount = 1;
+        }
+    }
+}
